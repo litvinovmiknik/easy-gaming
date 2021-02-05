1b9b4e2110daf63a044eb304dc521639f8ac03c2
Mikhail Litvinov <litvinovmiknik@gmail.com>
2021-02-05 21:35:54 +0500

    add page catalog

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,3 +2,4 @@ import '../blocks/nav/nav.js';
 import '../blocks/reviews-slider/reviews-slider.js';
 import '../blocks/top-games/top-games.js';
 import '../blocks/last-news/last-news.js';
+import '../blocks/catalog/catalog.js';
